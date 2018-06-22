049901c88f1dd60fdd955c03c890499b4462863a
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-06-22 16:38:20 -0200

    Added Form and Hooked up to Webpage

--- form_app/forms.py
+++ form_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class MyFirstForm(forms.Form):
-    name = forms.Charfield()
+    name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
