438758e60f8e132aebec0c112129273e42487aab
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-06-23 10:16:32 -0200

    Made a Bot Catcher

--- form_app/forms.py
+++ form_app/forms.py
@@ -4,3 +4,15 @@ class MyFirstForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,
+                                    widget=forms.HiddenInput)
+
+#these validator functions which raise an error should have their names NECESSARILY start with 'clean_'
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+
+        if len(botcatcher):
+            raise forms.ValidationError("GOTCHA BOT SMD!")
+
+
+        return botcatcher
