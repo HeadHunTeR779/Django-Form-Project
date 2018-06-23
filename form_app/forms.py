cc042ac050b9f48c9664cdb66a29d8d09cd07086
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-06-23 12:34:35 -0200

    Customized inbuilt validators

--- form_app/forms.py
+++ form_app/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from django.core import validators    #Django's inbuilt validators!
+from django.core import validators  #Django's inbuilt validators!
+
+
+
+def check_for_z(input_str):
+    if input_str[0].lower() != 'z':
+        raise forms.ValidationError('Name Needs to Start With Z')
 
 class MyFirstForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
     #required=False means no need to be filled by user HiddenInput means it will be in background HTML but won't be shown on the page
@@ -20,10 +26,8 @@ class MyFirstForm(forms.Form):
 #these validator functions which raise an error should have their names NECESSARILY start with 'clean_'
 #    def clean_botcatcher(self):
 #        botcatcher = self.cleaned_data['botcatcher']
-
 #        if len(botcatcher):
 #            raise forms.ValidationError("GOTCHA BOT SMD!")
-#
+
 
 #        return botcatcher
-#
