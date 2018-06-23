d470db3aa9a71758ae309511a78426ca1d0a9653
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-06-23 13:28:23 -0200

    Made a Master Function for Validation

--- form_app/forms.py
+++ form_app/forms.py
@@ -3,21 +3,20 @@ from django.core import validators  #Django's inbuilt validators!
 
 
 
-def check_for_z(input_str):
-    if input_str[0].lower() != 'z':
-        raise forms.ValidationError('Name Needs to Start With Z')
-
 class MyFirstForm(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    re_email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-    #required=False means no need to be filled by user HiddenInput means it will be in background HTML but won't be shown on the page
-    botcatcher = forms.CharField(required=False,
-                                    widget=forms.HiddenInput,
-                                    validators=[validators.MaxLengthValidator(0)])
 
-                                #that validators.MaxLengthValidator(0) does same job as below <3
 
+    def clean(self):     # just clean so its for validation of all the fields in the form <3
+        all_cleaned_data = super().clean()
+        email = all_cleaned_data['email']
+        re_email = all_cleaned_data['re_email']
+
+        if email != re_email:
+            raise forms.ValidationError("Make Sure Mails Match.")
 
 
 
